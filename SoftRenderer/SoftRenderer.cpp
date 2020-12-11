d21696387d6b9f5af53e7c2484fd0d59297f55f7
carlself <carlwang20150101@gmail.com>
2020-12-11 16:36:00 +0800

    tangent space normal

--- SoftRenderer/SoftRenderer.cpp
+++ SoftRenderer/SoftRenderer.cpp
@@ -17,6 +17,7 @@ vec3 eye(1,1, 3);
 vec3 center(0, 0, 0);
 
 Model* model;
+TGAImage* gridImage;
 //void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 //{
 //    for (float t = 0; t < 1; t += 0.01)
@@ -92,32 +93,36 @@ class Shader : public IShader
 public:
     //vec3 varying_intensities;
     mat<2, 3> varying_uvs;
-    mat<3, 3> varyings_world_pos;
-    mat<4, 4> uniform_MIT;
+    mat<3, 3> varing_norm;
+    mat<3, 3> varying_ndc;
     vec4 vertex(int face, int vertex) override
     {
         vec3 v = model->vert(face, vertex);
-        //varyings_world_pos.set_col(vertex, v);
-        //varyings_world_pos.set_col(vertex, proj<3>(ModelView * embed<4>(v)));
+        vec4 gl_vertex = Viewport * Projection * ModelView * embed<4>(v);
         varying_uvs.set_col(vertex, model->uv(face, vertex));
-        return Viewport * Projection * ModelView * embed<4>(v);
+        varing_norm.set_col(vertex, proj<3>((Projection * ModelView).invert_transpose() * embed<4>(model->normal(face, vertex), 0.0f)));
+        varying_ndc.set_col(vertex, proj<3>(gl_vertex / gl_vertex[3]));
+        return gl_vertex;
     }
 
     bool fragment(vec3 bc, TGAColor &color) override
     {
-        //float intensity = varying_intensities * bc;
-        //vec3 fragPos = varyings_world_pos * bc;
         vec2 uv = varying_uvs *bc;
-        vec3 n = proj<3>(uniform_MIT * embed<4>(model->normal(uv), 0)).normalize();
-        vec3 l =  proj<3>(Projection * ModelView * embed<4>(light_dir)).normalize();
-        vec3 r = (2 * (l * n) * n - l).normalize();
+        vec3 bn = (varing_norm * bc).normalize();
 
-        float spec = pow(std::max(r.z, 0.0), model->specular(uv));
-        float diff = std::max(0.0, n * l);
-        TGAColor c = model->diffuse(uv);
-        color = c;
-        for (int i = 0; i < 3; i++) color[i] = std::min<float>(5 + c[i] * (diff + .6 * spec), 255);
+        mat<3, 3> A = {varying_ndc.col(1) - varying_ndc.col(0), varying_ndc.col(2) - varying_ndc.col(0), bn };
+        mat<3, 3> AI = A.invert();
+        vec3 tangent = AI * vec3(varying_uvs[0][1] - varying_uvs[0][0], varying_uvs[0][2] - varying_uvs[0][0], 0.0);
+        vec3 bitangent = AI * vec3(varying_uvs[1][1] - varying_uvs[1][0], varying_uvs[1][2] - varying_uvs[1][0], 0.0);
 
+        mat<3, 3> m;
+        m.set_col(0, tangent.normalize());
+        m.set_col(1, bitangent.normalize());
+        m.set_col(2, bn);
+
+        vec3 n = proj<3>(ModelView * embed<4>((m * model->normal(uv)), 0.0)).normalize();
+        float diff = std::max(0.0, n * light_dir);
+        color = model->diffuse(uv) * diff;
 
        return false;
     }
@@ -139,8 +144,9 @@ int main(int argc, char** argv)
     viewport(0, 0, width, height, depth);
 
     Shader shader;;
-    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
 
+    gridImage = new TGAImage();
+    gridImage->read_tga_file("obj/grid.tga");
     TGAImage image(width, height, TGAImage::RGB);
     TGAImage depth(width, height, TGAImage::GRAYSCALE);
     for (int i = 0; i < model->nfaces(); i++)
